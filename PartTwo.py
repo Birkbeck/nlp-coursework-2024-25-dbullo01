4e53937736ebae4c57d795905062c2e645670a8f
Davison Bullock <dbullo01@student.bbk.ac.uk>
2025-06-07 10:14:27 +0100

    07-06-2025 - Ammended Implementation for Question 2 part a to not display dataframes (commented out) after each df transformation and also just to only return (print) dimensions of resulting dataframe

--- PartTwo.py
+++ PartTwo.py
@@ -18,7 +18,8 @@ import numpy as np
 # (a) Read the handsard40000.csv dataset in the texts directory into a dataframe. Subset and rename the dataframe as follows;
 # REF https://pandas.pydata.org/docs/reference/api/pandas.set_option.html
 df = pd.read_csv('C:\\Users\\db570\\PycharmProjects\\NLP_Coursework_2025\\nlp-coursework-2024-25-dbullo01\\p2-texts\\p2-texts\\hansard40000.csv')
-print(df)
+# print(df)  #PLEASE UNCOMMENT IF YOU WOULD LIKE TO DISPLAY DATAFRAME
+
 pd.set_option('display.max_columns', None)      # Display all columns. None - unlimited
 pd.set_option('display.max_rows', None)         # Display all rows. None - unlimited
 pd.set_option('display.width', None)            # Display width in characters for pandas. None - auto detects width
@@ -27,7 +28,7 @@ pd.set_option('display.width', None)            # Display width in characters fo
 # (i) rename the 'Labour (Co-op)' value in 'party' column to 'Labour', and then:   [DONE]
 #REF https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.replace.html
 df['party'] = df['party'].replace('Labour (Co-op)', 'Labour')
-display(df)
+# display(df) #PLEASE UNCOMMENT IF YOU WOULD LIKE TO DISPLAY DATAFRAME
 
 # (ii) remove any rows where the value of the 'party' column is not one of the four most common party names and remove
 #       the 'Speaker' value. [DONE]
@@ -35,16 +36,17 @@ display(df)
 # Get the frequency counts of each party
 #REF https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.value_counts.html
 party_counts = df['party'].value_counts()     # Descending order returns list of counts for each party
-display(party_counts)
+# display(party_counts)  #PLEASE UNCOMMENT IF YOU WOULD LIKE TO DISPLAY DATAFRAME
 
 # Get the names of the 4 most common parties from list  [DONE]
 #REF https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.value_counts.html
 #REF https://en.moonbooks.org/Articles/How-to-extract-the-value-names-and-counts-from-valuecounts-in-pandas-/
 
 party_names_list = df['party'].value_counts().index.tolist()
-print("The top 4 common parties are " + str(party_names_list[0] + "," + str(party_names_list[1] + \
-                            "," + str(party_names_list[2] ) + "," + str(party_names_list[4] ))))
-
+# print("The top 4 common parties are " + str(party_names_list[0] + "," + str(party_names_list[1] + \
+#                            "," + str(party_names_list[2] ) + "," + str(party_names_list[4] ))))
+# Code above used to determine the 4 common parties to then remove rows from dataframe in the next step that
+# are not those parties or the Speaker party
 
 # Now can remove the records from dataframe that contain non-common parties and also where party is Speaker [DONE]
 #REF https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.drop.html
@@ -61,10 +63,7 @@ df = df.drop(df[(df['party'] == 'Democratic Unionist Party') | (df['party'] == '
 #REF https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.drop.html
 #REF https://www.geeksforgeeks.org/drop-rows-from-the-dataframe-based-on-certain-condition-applied-on-a-column/
 df = df.drop(df[(df['speech_class'] != 'Speech')].index)
-display(df)
-
-print()
-print("Dimensions of the resulting dataframe is :" + str(df.shape))
+#display(df) - PLEASE UNCOMMENT FOR DISPLAY/DEBUG
 
 # (iv) remove any rows where the text in the 'speech' column is less than 1000 characters long [DONE]
 #REF https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.drop.html
@@ -74,10 +73,9 @@ print("Dimensions of the resulting dataframe is :" + str(df.shape))
 #print(df.speech.str.len())  - PLEASE UNCOMMENT IF YOU WOULD LIKE TO CHECK LENGTHS OF SPEECH TEXT
 
 df = df.drop(df[((df.speech.str.len() < 1000))].index)
-print(df)
+#display(df) - PLEASE UNCOMMENT FOR DISPLAY/DEBUG
 
 
 # Print the dimensions of the resulting dataframe using the shape method
 #REF https://pandas.pydata.org/docs/reference/api/pandas.DataFrame.shape.html
-print()
-print("Dimensions of the dataframe is :" + str(df.shape))
+print(str(df.shape))
