3b62eea317d792f34e92aff12f4b15d76c532c98
Davison Bullock <dbullo01@student.bbk.ac.uk>
2025-06-23 14:32:05 +0100

    23/06/2025 Ammended tokenize_text2() by adding comment to above the function to give more context

--- PartTwo.py
+++ PartTwo.py
@@ -212,7 +212,7 @@ def tokenize_text(text):
 
 def tokenize_text2(text):
     """ custom tokenizer that preprocess text for input to the TftdfVectorizer including calling an improved
-        stemmer function called stemmer2(). Stemmer2() stems each word in text
+        stemmer function called stemmer2(). Stemmer2() stems each word in text and improved classification results
 
         Args:
             text: text to clean and tokenize
